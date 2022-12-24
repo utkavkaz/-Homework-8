ac8ba7d44ac35018272e2c281aff91a70e28503b
utkavaz <ntkavkaz@rambler.ru>
2022-12-24 22:08:19 +0300

    homework

---
+++ Z_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int M = 4, N = 8;
+
+int SummaRec(int M, int N)
+{
+    if (N < M) return 0;
+    return N + SummaRec(M, N - 1);
+}
+
+Console.Write("Введите целое число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от {M} до {N} равна {SummaRec(M, N)}");
---
+++ Z_68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// n = 2, m = 3 -> A(n,m) = 9
+// n = 3, m = 2 -> A(n,m) = 29
+
+int Akk(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+
+Console.Write("Введите целое число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akk(n, m));
